2e05cf34d93e3c15d4f82eb9109a7203379cab33
Laura-Oliveira <laura.regina.cp@gmail.com>
2023-08-21 17:57:00 -0300

    [Create] Utilizacao dos elementos Box, Column e Row

--- app/src/main/java/com/jetpack_compose_api/MainActivity.kt
+++ app/src/main/java/com/jetpack_compose_api/MainActivity.kt
@@ -5,16 +5,22 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
-import androidx.compose.ui.semantics.Role.Companion.Image
 import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
 class MainActivity : AppCompatActivity() {
@@ -23,8 +29,10 @@ class MainActivity : AppCompatActivity() {
         setContent {
            //setUsername()
           //  setPhoto()
-            setProfileData()
+           // setProfileData()
            // setExtraInfo()
+
+            ProfileScreen()
         }
     }
 }
@@ -100,6 +108,79 @@ fun setExtraInfo()
                 fontSize = 35.sp,
                 fontFamily = FontFamily.SansSerif,
             )
+    }
+}
+
+@Composable
+fun ProfileScreen() {
+    /*  Column {
+        Image(
+            painterResource(id = R.mipmap.ic_photo_foreground),
+            contentDescription = "profile pic"
+        )
+        Text("Laura Oliveira")
+        Text("@Laura-Oliveira")
+        Text("Mobile Developer | Android | iOS | Java | Kotlin | SwiftUI")
+    }
+*/
+    Column {
+
+
+        val boxHeight = remember {
+            180.dp
+        }
+        val imageHeight = remember {
+            boxHeight
+        }
+        Box(
+            modifier = Modifier
+                .fillMaxWidth()
+                .background(
+                    Color(R.color.purple_500), shape = RoundedCornerShape(
+                        bottomStart = 16.dp,
+                        bottomEnd = 16.dp
+                    )
+                )
+                .height(boxHeight)
+        ) {
+            Image(
+                painterResource(id = R.drawable.profile),
+                contentDescription = "profile pic",
+                modifier = Modifier
+                    .offset(y = imageHeight / 2)
+                    .size(imageHeight)
+                    .align(Alignment.BottomCenter)
+                    .clip(CircleShape),
+            )
+        }
+        Spacer(modifier = Modifier.height(imageHeight / 2))
+        Column(
+            Modifier
+                .padding(8.dp)
+                .fillMaxWidth(),
+            horizontalAlignment = Alignment.CenterHorizontally
+        ) {
+            Text(
+                "Laura Oliveira",
+                fontSize = 32.sp
+            )
+            Text(
+                "@Laura-Oliveira",
+                fontSize = 18.sp,
+                fontWeight = FontWeight.Bold
+            )
+        }
+        Text(
+            "Mobile Developer | Android | iOS | Java | Kotlin | SwiftUI",
+            Modifier
+                .padding(
+                    start = 8.dp,
+                    bottom = 8.dp,
+                    end = 8.dp
+                )
+                .fillMaxWidth(),
+            textAlign = TextAlign.Center
+        )
     }
 }
 
